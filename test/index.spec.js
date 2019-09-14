8af9d39d35e04df26d30662bdfd6d43847d2d74a
Marco Heins <marco.heins@posteo.de>
2019-09-14 13:43:43 +0200

    +test categories: arrays, objects...

--- test/index.spec.js
+++ test/index.spec.js
@@ -9,30 +9,34 @@ describe('30 seconds of code TDD', function() {
       true.should.be.true
     })
   })
-  describe('all(array, condition)', function() {
-    it('should return true if all elements match condition', function() {
-      src.all([2, 3, 4], x => x > 1).should.be.true
+  describe('Arrays:', function() {
+    describe('all(array, condition)', function() {
+      it('should return true if all elements match condition', function() {
+        src.all([2, 3, 4], x => x > 1).should.be.true
+      })
+    })
+    describe('any(array, condition)', function() {
+      it('shoud return true if any element matches condition', function() {
+        src.any([0, 1, 2], x => x > 1).should.be.true
+      })
+    })
+    describe('removeFalsy', function() {
+      it('shoud remove falsy elements from array', function() {
+        src
+          .removeFalsy([null, NaN, '', undefined, {}, 0, 1, 'a'])
+          .should.be.eql([{}, 1, 'a'])
+      })
     })
   })
-  describe('any(array, condition)', function() {
-    it('shoud return true if any element matches condition', function() {
-      src.any([0, 1, 2], x => x > 1).should.be.true
+  describe('Objects:', function() {
+    describe('isEmptyObject', function() {
+      it('should only be true if it has no properties ', function() {
+        src.isEmptyObject({}).should.be.true
+        src.isEmptyObject({ a: 1 }).should.be.false
+      })
     })
-  })
-  describe('removeFalsy', function() {
-    it('shoud remove falsy elements from array', function() {
-      src
-        .removeFalsy([null, NaN, '', undefined, {}, 0, 1, 'a'])
-        .should.be.eql([{}, 1, 'a'])
+    describe('', function() {
+      it('', function() {})
     })
-  })
-  describe('isEmptyObject', function() {
-    it('should only be true if an object has no properties ', function() {
-      src.isEmptyObject({}).should.be.true
-      src.isEmptyObject({ a: 1 }).should.be.false
-    })
-  })
-  describe('', function() {
-    it('', function() {})
   })
 })
