e96af5de28e15bf5852fe6e3f7a2cf8a831dfa89
Marco Heins <marco.heins@posteo.de>
2019-09-14 13:22:02 +0200

    +removeFalsy() +isEmptyObject()

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ var exports = (module.exports = {})
 
 exports.all = (arr, fn = Boolean) => arr.every(fn)
 exports.any = (arr, fn = Boolean) => arr.some(fn)
+exports.removeFalsy = arr => arr.filter(Boolean)
+exports.isEmptyObject = obj => Object.keys(obj).toString() === ''
