e0b1c4a325298b8910b61c800981e6fcae66e99e
Marco Heins <marco.heins@posteo.de>
2019-09-14 15:02:25 +0200

    index: categories fixed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@
 
 var exports = (module.exports = {})
 
+exports.equals = () => false
+
+// Arrays
 exports.all = () => false
 exports.any = () => false
 exports.removeFalsy = () => false
+
+// Objects
 exports.isEmptyObject = () => false
